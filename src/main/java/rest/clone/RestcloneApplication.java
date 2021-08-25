48c38e69b419cc85742617d42a948dfe1930f640
ymzkjpx <ymzkjpx@gmail.com>
2021-08-25 23:38:14 +0900

    users用のコントロラーを追加

--- src/main/java/rest/clone/RestcloneApplication.java
+++ src/main/java/rest/clone/RestcloneApplication.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(nameGenerator = FQCNGenerator.class)
 public class RestcloneApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/rest/clone/presentation/controller/newgreeting/GreetingController.java
@@ -0,0 +1,21 @@
+package rest.clone.presentation.controller.newgreeting;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import rest.clone.application.service.newgreeting.GreetingService;
+
+@RestController
+@RequestMapping("/api/users")
+public class GreetingController {
+
+    @Autowired
+    GreetingService greetingService;
+
+    @RequestMapping(value = "by/username/{id}", method = RequestMethod.GET)
+    String byUsername(@PathVariable("id") String id){
+        return id;
+    }
+}
