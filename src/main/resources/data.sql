ad531b55c37711fe7fb2eeeca30936436af77cc1
ymzkjpx <ymzkjpx@gmail.com>
2021-08-28 23:18:44 +0900

    messageスキーム初期データ投入

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,3 +15,20 @@ VALUES
   , (2, 'Bob')
   , (3, 'Carol')
 ;
+
+----------------------------- message
+
+INSERT INTO message.routing (message_id, sentence)
+VALUES
+    (1, 'HelloBob'),
+    (2, 'HelloCarol'),
+    (3, 'HelloAlice')
+;
+
+insert into message.messages
+(message_id, from_user_id, to_user_id)
+VALUES
+(1, 1, 2),
+(2, 2, 3),
+(3, 3, 1)
+;
